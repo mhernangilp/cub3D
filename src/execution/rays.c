45016c8badced37bf160c1710de361a5c9c99eee
Gonzalo <gonchogfa@gmail.com>
2024-03-10 23:28:33 +0100

    walls corrections side

--- src/execution/rays.c
+++ src/execution/rays.c
@@ -7,9 +7,11 @@ void	draw_rays2d(t_cub cub, t_ray ray)
 {
 	float	r;
 	t_brsh		brsh;
+	float	w;
 
-	r = 0.0;
-	while (r < 60.0)
+	r = 60.0;
+	w = 0.0;
+	while (r > 0.0)
 	{
 		ray.ra = cub.pa - cub.ray.angle + r;
 		vertical(cub, &ray);
@@ -25,12 +27,13 @@ void	draw_rays2d(t_cub cub, t_ray ray)
 		brsh.x1 = ray.rx;
 		brsh.y1 = ray.ry;
 		bresenham_line(cub, brsh, 0xFF0000);
-		brsh.x0 = r * 8 + 530;
+		brsh.x0 = w * 8 + 530;
   		brsh.y0 = (W_HEIGHT / 2) - ((MAP_SCALE * 320) / ray.d_h / 2);
-    	brsh.x1 = r * 8 + 530;
+    	brsh.x1 = w * 8 + 530;
     	brsh.y1 = brsh.y0 + (MAP_SCALE * 320) / ray.d_h;
     	bresenham_line(cub, brsh, 0x00FF00);
-		r += 0.1;
+		r -= 0.1;
+		w += 0.1;
 	}
 }
 
